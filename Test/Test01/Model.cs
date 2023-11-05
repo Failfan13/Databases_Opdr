ceabc1084b9d2b32416de3b34300d6c10c7c70d5
Erik Maan <failfan13gta5@gmail.com>
2023-11-05 17:13:35 +0100

    Fix adding migrations

--- Test/Test01/Model.cs
+++ Test/Test01/Model.cs
@@ -9,7 +9,8 @@ class HotelContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("User ID=postgres;Password=;Host=localhost;port=5432;Database=LocalPostgres;Pooling=true");
+        //optionsBuilder.UseNpgsql("User ID=postgres;Password=;Host=localhost;port=5432;Database=LocalPostgres;Pooling=true");
+        optionsBuilder.UseNpgsql(@"Host=localhost:5432;Username=postgres;Password=;Database=LocalDbtje;Pooling=true");
         //optionsBuilder.LogTo(System.Console.WriteLine, LogLevel.Critical);
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
